9638bb0d65d56c5d0091188fc592b5b986c7ca56
Setsuodu <339131001@qq.com>
2017-11-07 10:24:06 +0800

    unity project commit

---
+++ Unity/Assets/AlipayUI.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class PayInfo
+{
+    public string subject;  // 显示在按钮上的内容,跟支付无关系  
+    public float money;     // 商品价钱  
+    public string title;    // 商品描述  
+}
+
+public class AlipayUI : MonoBehaviour
+{
+    public Text logText, resultText;
+
+    public string className = "com.mier.mymirror.MyPluginClass";
+    public List<Button> buttons = null;
+    public List<PayInfo> payInfos = null;
+    private AndroidJavaObject jo = null;
+
+    void Start()
+    {
+        // Init UI
+        for (int i = 0; i < buttons.Count; i++)
+        {
+            var payInfo = payInfos[i];
+            buttons[i].GetComponentInChildren<Text>().text = payInfos[i].subject;
+#if UNITY_ANDROID && !UNITY_EDITOR
+            buttons[i].onClick.AddListener(() =>
+            {
+                Alipay(payInfo);
+            });
+#endif
+        }
+        // 固定写法
+        //AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        //jo = jc.GetStatic<AndroidJavaObject>("currentActivity");
+        //jo.Call("SayHello");
+
+        AndroidJavaClass jc = new AndroidJavaClass(className);
+        jo = jc.CallStatic<AndroidJavaObject>("GetInstance", gameObject.name); //Main Camera
+        jo.Call("SayHello"); //void SayHello()，没有返回类型
+
+        resultText.text = jo.Call<int>("CalculateAdd", 12, 34).ToString(); //int CalculateAdd()，有返回类型
+    }
+
+    // AlipayClient是Android里的方法名字，写死.
+    // payInfo.money是要付的钱，只能精确分.
+    // payInfo.title是商品描述信息，注意不能有空格.
+    //jo.Call("AlipayClient", payInfo.money, payInfo.title, "");
+    public void Alipay(PayInfo payInfo)
+    {
+        //jo.Call("Pay", "商品,详情,1.0元"); //void Pay()，没有返回类型
+        string res = jo.Call<string>("AlipayClient", "商品,详情,1.0元"); //void Pay()，没有返回类型
+        Debug.Log("[res]" + res);
+    }
+
+    public void PluginCallBack(string text)
+    {
+        logText.text = text;
+    }
+}
