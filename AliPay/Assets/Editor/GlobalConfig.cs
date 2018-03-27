63a6c200129536adb81f9bf8d31ac88d5d77f7f6
Setsuodu <339131001@qq.com>
2018-03-27 15:27:33 +0800

    auto add keystore

---
+++ AliPay/Assets/Editor/GlobalConfig.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+
+[InitializeOnLoad]
+public class GlobalConfig
+{
+    static GlobalConfig()
+    {
+        PlayerSettings.Android.keystorePass = "123456";
+        PlayerSettings.Android.keyaliasName = "mymirror";
+        PlayerSettings.Android.keyaliasPass = "123456";
+    }
+}
